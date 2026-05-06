6320e7cbcea959023f641d7aa14976aa770569a6
JxseDiaz <josediaz173006@gmail.com>
2026-05-05 20:38:24 -0400

    Creacion

---
+++ pet/src/test/java/cl/sanossalvos/pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sanossalvos.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
